1acda6cc24ea60ba74af20640b96d9e0f2b9a8a5
Jesus Cidoncha <jesus.cidoncha.berlanga@gmail.com>
2024-01-25 09:36:12 +0100

    routes fix + auth routes

--- routes/event.routes.js
+++ routes/event.routes.js
@@ -1,76 +1,79 @@
-
-const router = require('express').Router()
+const { isAuthenticated } = require("../middlewares/route-guard.middleware");
+const router = require("express").Router();
+const Event = require("../models/Event.model");
 
 // GET all
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
-    const allEvents = await Event.find()
-    res.status(200).json(allEvents)
+    const allEvents = await Event.find();
+    res.status(200).json(allEvents);
   } catch (error) {
-    console.log(error)
-    res.status(500).json({ message: 'error while getting the events' })
+    console.log(error);
+    res.status(500).json({ message: "error while getting the events" });
   }
-})
+});
 // GET one
-router.get('/:eventId', async (req, res) => {
-  const { eventId } = req.params
+router.get("/:eventId", async (req, res) => {
+  const { eventId } = req.params;
   try {
-    const oneEvent = await Event.findById(eventId)
-    res.status(200).json(oneEvent)
+    const oneEvent = await Event.findById(eventId);
+    res.status(200).json(oneEvent);
   } catch (error) {
-    console.log(error)
-    res.status(500).json({ message: 'error while getting the event' })
+    console.log(error);
+    res.status(500).json({ message: "error while getting the event" });
   }
-})
+});
 
 // POST one
-router.post('/', isAuthenticated, async (req, res) => {
-  const payload = req.body
-  const { userId } = req.tokenPayload
-  payload.createdBy = userId
+router.post("/", isAuthenticated, async (req, res) => {
+  const payload = req.body;
+  const { userId } = req.tokenPayload;
+  payload.createdBy = userId;
   try {
-    const createdEvent = await Event.create(payload)
-    res.status(201).json(createdEvent)
+    const createdEvent = await Event.create(payload);
+    res.status(201).json(createdEvent);
   } catch (error) {
-    console.log(error)
-    res.status(500).json({ message: 'error while creating the event' })
+    console.log(error);
+    res.status(500).json({ message: "error while creating the event" });
   }
-})
+});
 // PUT one
-router.put('/:eventId', isAuthenticated, async (req, res) => {
-  const { userId } = req.tokenPayload
-  const payload = req.body
-  const { eventIdId } = req.params
+router.put("/:eventId", isAuthenticated, async (req, res) => {
+  const { userId } = req.tokenPayload;
+  const payload = req.body;
+  const { eventId } = req.params;
   try {
-    const eventToUpdate = await Event.findById(eventId)
+    const eventToUpdate = await Event.findById(eventId);
     if (eventToUpdate.createdBy == userId) {
-      const updatedEvent = await Event.findByIdAndUpdate(eventId, payload, { new: true })
-      res.status(200).json(updatedEvent)
+      const updatedEvent = await Event.findByIdAndUpdate(eventId, payload, {
+        new: true,
+      });
+      res.status(200).json(updatedEvent);
     } else {
-      res.status(403).json({ message: 'you are not the right user' })
+      res.status(403).json({ message: "you are not the right user" });
     }
   } catch (error) {
-    console.log(error)
-    res.status(500).json({ message: 'error while updating the event' })
+    console.log(error);
+    res.status(500).json({ message: "error while updating the event" });
   }
-})
+});
 // DELETE one
-router.delete('/:eventId', isAuthenticated, async (req, res) => {
-  const { userId } = req.tokenPayload
-  const { eventId } = req.params
+router.delete("/:eventId", isAuthenticated, async (req, res) => {
+  const { userId } = req.tokenPayload;
+  const { eventId } = req.params;
   try {
-    const eventToDelete = await Event.findById(eventId)
-    console.log(eventToDelete, userId)
+    const eventToDelete = await Event.findById(eventId);
+    console.log(eventToDelete, userId);
     if (eventToDelete.createdBy == userId) {
-      console.log('Deleting')
-      await Event.findByIdAndDelete(eventId)
-      res.status(204).json()
+      console.log("Deleting");
+      await Event.findByIdAndDelete(eventId);
+      res.status(204).json();
     } else {
-      res.status(403).json({ message: 'you are not the right user' })
+      res.status(403).json({ message: "you are not the right user" });
     }
   } catch (error) {
-    res.status(500).json({ message: 'error while deleting the event' })
+    res.status(500).json({ message: "error while deleting the event" });
   }
-})
+});
 
-module.exports = router
+module.exports = router;
